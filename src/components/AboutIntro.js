9c97d57a6394d6d302471c5713c7eb46b7d658e5
Mark Bedner <bednermb3420@gmail.com>
2021-08-05 21:30:03 -0400

    new files created and updated

---
+++ src/components/AboutIntro.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import MainBtn from './Buttons/MainBtn'
+import bgShapes from '../assets/bg-shapes2.svg'
+import {
+    Link
+  } from "react-router-dom";
+
+function AboutIntro() {
+    return (
+        <section className="container m-auto relative">
+            <div className="flex items-center justify-center gap-20 flex-row-reverse">
+                <div className="w-1/2 relative z-10">
+                    <h2 className="text-4xl font-heading mb-8 text-blue-extradark capitalize leading-normal">Lorem ipsum adipisicing explicabo consectetu</h2>
+                    <p className="opacity-80 my-4 font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam enim explicabo aut vero quia, perferendis minus adipisci facilis unde tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam enim explicabo aut vero quia, perferendis minus adipisci facilis unde tenetur</p>
+                    <Link to="/about"><MainBtn>Learn About Us</MainBtn></Link>
+                </div>
+                <div className="w-1/2 flex items-center justify-center p-14">
+                    <div className="bg-gradient-to-br from-blue-dark to-blue-medium rounded-lg w-full h-80 relatie">
+                        <img src={bgShapes} className="object-contain absolute left-0 opacity-60 mix-blend-screen z-0 transform" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AboutIntro
--- src/components/Features.js
+++ src/components/Features.js
@@ -3,51 +3,51 @@ import MainBtn from './Buttons/MainBtn'
 
 function Features() {
     return (
-<section className="container m-auto py-28 relative">
-                <div className="flex">
-                    <div className="w-1/3 pr-16">
-                        <h2 className="text-4xl font-heading mb-8 text-blue-extradark">Test Name</h2>
-                        <p className="opacity-80 my-4 font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam enim explicabo aut vero quia, perferendis minus adipisci facilis unde tenetur.</p>
-                        <MainBtn>Contact Us</MainBtn>
-                    </div>
-                    <div className="w-2/3">
-                        <div className="grid grid-cols-3 gap-8 mb-8">
-                            <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
-                                <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
-                                <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
-                                <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
-                            </div>
-                            <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
-                                <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
-                                <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
-                                <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
-                            </div>
-                            <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
-                                <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
-                                <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
-                                <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
-                            </div>
+        <section className="container m-auto py-28 relative">
+            <div className="flex">
+                <div className="w-1/3 pr-16">
+                    <h2 className="text-4xl font-heading mb-8 text-blue-extradark capitalize leading-normal">Ipsum Dolor Sit Amet consectetur</h2>
+                    <p className="opacity-80 my-4 font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam enim explicabo aut vero quia, perferendis minus adipisci facilis unde tenetur.</p>
+                    <MainBtn>Contact Us</MainBtn>
+                </div>
+                <div className="w-2/3">
+                    <div className="grid grid-cols-3 gap-8 mb-8">
+                        <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
+                            <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
+                            <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
+                            <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
                         </div>
-                        <div className="grid grid-cols-3 gap-8">
-                            <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
-                                <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
-                                <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
-                                <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
-                            </div>
-                            <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
-                                <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
-                                <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
-                                <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
-                            </div>
-                            <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
-                                <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
-                                <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
-                                <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
-                            </div>
+                        <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
+                            <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
+                            <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
+                            <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
+                        </div>
+                        <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
+                            <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
+                            <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
+                            <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-3 gap-8">
+                        <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
+                            <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
+                            <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
+                            <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
+                        </div>
+                        <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
+                            <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
+                            <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
+                            <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
+                        </div>
+                        <div className="bg-white rounded-md p-8 shadow-long border-b-4 border-blue-medium">
+                            <h3 className="leading-tight font-heading text-blue-extradark my-4 text-2xl">Lorem Ipsum Dolor</h3>
+                            <div className="w-12 h-1 bg-gradient-to-r from-yellow-dark to-yellow-medium my-2"></div>
+                            <p className="opacity-80 my-4 text-sm font-body text-blue-extradark">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus deleniti ullam.</p>
                         </div>
                     </div>
                 </div>
-            </section>
+            </div>
+        </section>
     )
 }
 
