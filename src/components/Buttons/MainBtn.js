7c0788a2ac13a0de94fd47cf62f4c2d5ae674397
Mark Bedner <bednermb3420@gmail.com>
2021-08-05 21:33:28 -0400

    removing render as from MainBtn file

--- src/components/Buttons/MainBtn.js
+++ src/components/Buttons/MainBtn.js
@@ -5,7 +5,7 @@ import { IconContext } from "react-icons";
 
 function MainBtn(props) {
     return (
-        <button renderAs="button" className="px-6 py-4 uppercase rounded-lg bg-gradient-to-b from-yellow-medium to-yellow-dark text-blue-extradark  mt-12 mb-8 shadow shadow-inner tracking-widest font-heading text-sm flex items-center justify-center gap-2">
+        <button className="px-6 py-4 uppercase rounded-lg bg-gradient-to-b from-yellow-medium to-yellow-dark text-blue-extradark  mt-12 mb-8 shadow shadow-inner tracking-widest font-heading text-sm flex items-center justify-center gap-2">
             {props.children} 
             <IconContext.Provider value={{ color: "text-blue-extradark", className: "w-5 h-5" }}>
                 <HiChevronRight/>
